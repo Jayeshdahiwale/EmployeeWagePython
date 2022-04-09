137f3f4848ea2d08850c4b85ee58ce2a170eb363
JAYESH <jayjdahivale@gmail.com>
2022-04-09 14:46:20 +0530

    [Jayesh]:Added welcome message

---
+++ employee_wage.py
@@ -0,0 +1,13 @@
+'''
+@Author: Jayesh Dahiwale
+@Date: 2022-04-09 18:00:30
+@Last Modified by: Jayesh Dahiwale
+@Last Modified time: 2021-02-11 18:00:30
+@Title : EmployeeWage Assignment
+'''
+
+class Employee:
+    def __init__(self):
+        pass
+
+print("Welcome to EmployeeWage computation program")

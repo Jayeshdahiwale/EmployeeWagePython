e1b69b86488f01487a76d2df6b352698563ce31f
JAYESH <jayjdahivale@gmail.com>
2022-04-11 09:27:27 +0530

    [Jayesh]:Refactored the code and made some changes

--- employee_wage.py
+++ employee_wage.py
@@ -1,22 +1,24 @@
-'''
+"""
 @Author: Jayesh Dahiwale
 @Date: 2022-04-09 18:00:30
 @Last Modified by: Jayesh Dahiwale
 @Last Modified time: 2021-02-11 18:00:30
 @Title : EmployeeWage Assignment
-'''
+"""
 
 import random
 
+
 class Employee:
     def __init__(self):
-        pass
+        self.attendance = random.randint(0, 1)
+
     def check_attendance(self):
-        attendance = random.randint(0,1)
-        if attendance == 0:
-            print("Employee is present")
-        else:
+        if self.attendance == 0:
             print("Employee is absent")
+        else:
+            print("Employee is present")
+
 
 print("Welcome to EmployeeWage computation program")
 employee = Employee()

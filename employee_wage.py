63aff9ff66b98fe9767b5250fed8b655cb974635
JAYESH <jayjdahivale@gmail.com>
2022-04-11 10:17:10 +0530

    [Jayesh]:Refactored the code and added part time employee and wage

--- employee_wage.py
+++ employee_wage.py
@@ -14,31 +14,46 @@ class Employee:
     def __init__(self):
         self.wage_per_hour = 20
         self.full_day_hour = 8
+        self.part_time_hour = 4
         self.attendance = random.randint(0, 1)
+        self.status = 0
 
     """
        Below functions checks whether the employee is present or absent. And print the status
     """
+
     def check_attendance(self):
         if self.attendance == 0:
             print("Employee is absent")
         else:
-            print("Employee is present")
+            self.check_employee_status()
+    """
+    Below function check whether the full time or part time employee present
+    """
+    def check_employee_status(self):
+        self.status = random.randint(0, 1)
+        if self.status == 0:
+            print("Part time employee present")
+        else:
+            print("Full time employee present")
+
 
     """
     Below function  calculates the daily employee wage according to his attendance
     """
+
     def daily_emp_wage(self):
         if self.attendance == 0:
             daily_wage = 0
         else:
-            daily_wage = self.wage_per_hour * self.full_day_hour
+            if self.status == 0:
+                daily_wage = self.wage_per_hour * self.part_time_hour
+            else:
+                daily_wage = self.wage_per_hour * self.full_day_hour
         print(f"The daily employee wage is Rs.{daily_wage}")
-
 
 
 print("Welcome to EmployeeWage computation program")
 employee = Employee()
 employee.check_attendance()
 employee.daily_emp_wage()
-
